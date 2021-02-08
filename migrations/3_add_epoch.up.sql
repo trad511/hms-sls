9b44e9f5258116cf5446955b6d1ab3ef4c24b7c1
Andrew Nieuwsma <andrew.nieuwsma@hpe.com>
2021-02-08 14:20:47 -0600

    fixed copyrights in sql

--- migrations/3_add_epoch.up.sql
+++ migrations/3_add_epoch.up.sql
@@ -21,14 +21,7 @@
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  */
---
--- Except as permitted by contract or express written permission of Cray Inc.,
--- no part of this work or its content may be modified, used, reproduced or
--- disclosed in any form. Modifications made without express permission of
--- Cray Inc. may damage the system the software is installed within, may
--- disqualify the user from receiving support from Cray Inc. under support or
--- maintenance contracts, or require additional support services outside the
--- scope of those contracts to repair the software or system.
+
 
 ALTER TABLE components
     ADD last_updated_version bigint NOT NULL
